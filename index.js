69982152d15ed1bade8ba165f556ccab2630cffa
Tarun-9313 <tarunbalaji1@gmail.com>
2025-09-29 13:10:26 +0530

    added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 var express =require("express")
 var cors=require("cors")
 
-var router = require("./routes/book-routes")
+var bookrouter = require("./routes/book-routes")
 var productrouter = require("./routes/product-routes")
 
 var  ConnectToDatabase = require("./database/db")
@@ -19,7 +19,7 @@ ConnectToDatabase()
 
 app.use(express.json())
 app.use(cors())
-app.use("/api/books",router)
+app.use("/api/books",bookrouter)
 app.use("/api/products",productrouter)
 
 var PORT = process.env.PORT || 3000
